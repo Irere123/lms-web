6dbb0917a16cce894b500038af89094d10575139
Irere123 <irere2020@outlook.com>
2024-05-05 09:46:58 +0200

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,17 +11,33 @@ export default function LoginPage() {
     >
       <div className="hidden sm:flex" />
       <div className="flex justify-self-center self-center sm:hidden">
-        <h1>Hello</h1>
+        <h1 className="text-slate-900">LMS</h1>
       </div>
       <div className="flex m-auto flex-col p-6 gap-5 bg-slate-800 sm:rounded-8 z-10 sm:w-400 w-full">
         <h2 className="text-white">Welcome back</h2>
         <div className="flex flex-col gap-3">
           <p className="text-slate-50">
-            By preceeding you agree to our terms of use and privacy policies
+            By preceeding you agree to our terms of use and privacy policy
           </p>
           <Input placeholder="Email" />
           <Input placeholder="Password" />
           <Button color="primary">Login</Button>
+        </div>
+      </div>
+      <div className="flex flex-row absolute bottom-0 w-full justify-between px-5 py-5 mt-auto items-center sm:px-7">
+        <div className="hidden sm:flex">
+          <h2>LMS</h2>
+        </div>
+        <div className="flex flex-row gap-6 text-slate-900">
+          <a href="/privacy-policy.html" className="hover:text-slate-800">
+            Privacy policy
+          </a>
+          <a
+            href="https://github.com/benawad/dogehouse/issues"
+            className="ml-2 hover:text-slate-800"
+          >
+            Report a bug
+          </a>
         </div>
       </div>
     </div>
