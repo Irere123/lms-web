9c96967b0d14d4821809f7e236b3790d28f0362c
Irere123 <irere2020@outlook.com>
2024-05-05 08:47:00 +0200

    bootstrap setup

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,18 @@ function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <div className="card">
+        <div className="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
